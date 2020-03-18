977c91edb49d87f8bc5c4b0f46c9213c209278f7
Sitabja <sitabjapal03@gmail.com>
2020-03-18 17:19:20 +0530

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('Client side javascript file is loaded!')
+
+
+const weatherForm=document.querySelector('form')
+weatherForm.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    document.getElementById("message-1").innerHTML="Loading..."
+    //console.log("Testing")
+    const location=document.querySelector('input').value
+    //console.log(location)
+    const url='http://localhost:3000/weather?address='+location
+    fetch(url).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                document.getElementById("message-1").innerHTML=data.error
+                document.getElementById("message-2").innerHTML=''
+                //console.log(data.error)
+            }else{
+                //else console.log(data)
+                document.getElementById("message-1").innerHTML=data.location
+                document.getElementById("message-2").innerHTML=data.forecast
+            }
+        })
+    })
+})
